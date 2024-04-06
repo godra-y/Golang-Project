c04bceb28fee3c6bd5e1fd0dae463ac88326fe51
godra-y <122892394+godra-y@users.noreply.github.com>
2024-04-06 11:38:02 +0500

    healthcheck, helpers

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
 )
+
+type envelope map[string]interface{}
 
 func (app *application) readIDParam(r *http.Request) (int, error) {
 	vars := mux.Vars(r)
@@ -17,4 +20,21 @@ func (app *application) readIDParam(r *http.Request) (int, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	// Use the json.MarshalIndent() function so that whitespace is added to the encoded
+	// JSON. Here we use no line prefix ("") and tab indents ("\t") for each element.
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
